4d9bbe32e299adc10030bcafef5f54fcb82e14da
rubyciide <joelpaul900@gmail.com>
2023-07-02 19:50:31 +0530

    changeArc

--- assets/script.js
+++ assets/script.js
@@ -43,28 +43,34 @@ let humanElement,computerElement;
 let humanScore = 0,computerScore = 0;
 let rock = document.getElementById('rock');
 console.log(rock)
-rock.addEventListener("click",()=>{
-    console.log("rock clicked")
-    humanElement = "rock";
-    computerElement = elements[Math.floor(Math.random()*3)];
-    console.log(humanElement,computerElement)
-    results.textContent=winner();
-    updateScore(winner());
+while(humanScore <=5 && computerScore<=5){
+    mechanics();
+}
 
-})
-paper.addEventListener("click",()=>{
-    console.log("rock clicked")
-    humanElement = "paper";
-    computerElement = elements[Math.floor(Math.random()*3)];
-    console.log(humanElement,computerElement)
-    results.textContent=winner();
-    updateScore(winner());
-})
-scissors.addEventListener("click",()=>{
-    console.log("rock clicked")
-    humanElement = "scissors";
-    computerElement = elements[Math.floor(Math.random()*3)];
-    console.log(humanElement,computerElement)
-    results.textContent=winner();
-    updateScore(winner());
-})
+function mechanics(){
+    rock.addEventListener("click",()=>{
+        console.log("rock clicked")
+        humanElement = "rock";
+        computerElement = elements[Math.floor(Math.random()*3)];
+        console.log(humanElement,computerElement)
+        results.textContent=winner();
+        updateScore(winner());
+    
+    })
+    paper.addEventListener("click",()=>{
+        console.log("rock clicked")
+        humanElement = "paper";
+        computerElement = elements[Math.floor(Math.random()*3)];
+        console.log(humanElement,computerElement)
+        results.textContent=winner();
+        updateScore(winner());
+    })
+    scissors.addEventListener("click",()=>{
+        console.log("rock clicked")
+        humanElement = "scissors";
+        computerElement = elements[Math.floor(Math.random()*3)];
+        console.log(humanElement,computerElement)
+        results.textContent=winner();
+        updateScore(winner());
+    })
+}
